209637833cff7da42f02b2f0afb46764474b4beb
do3aam <doaam7555@gmail.com>
2021-09-05 17:43:52 +0200

    Change bottom@paragraph

--- Source-bootstrap-5.1.0-dist/js/script.js
+++ Source-bootstrap-5.1.0-dist/js/script.js
@@ -1,7 +1,8 @@
 var quotes = [
     {proverb:"Be yourself; everyone else is already taken." , author: "-- Oscar Wilde"},
     {proverb:"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best." , author: "-- Marilyn Monroe"},
-    {proverb:"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe." , author:" -- Albert Einstein" },
+    {proverb:"You've gotta dance like there's nobody watching,Love like you'll never be hurt, Sing like there's nobody listening, And live like it's heaven on earth." ,
+    author:"William W. Purkey" },
 ];
 
 
